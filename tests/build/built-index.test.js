42ba4d7369a002c64cb90275caa8fde8bfe2aa48
Sir-Kyle-Richardson <richardson.a.kyle.95@gmail.com>
2021-09-16 07:45:12 -0400

    more testing of index.ts

--- tests/build/built-index.test.js
+++ tests/build/built-index.test.js
@@ -1,7 +1,7 @@
-import * as jsUtil from '../index';
+const jsUtil = require('../../build/index');
 
-describe('index', () => {
-    test('exports all modules from src', () => {
+describe('js-util', () => {
+    test('exports all', () => {
         expect(jsUtil.constructor.name).toBe('Object');
         // @ts-ignore
         expect(jsUtil.peek([1, 2])).toBe(2);
