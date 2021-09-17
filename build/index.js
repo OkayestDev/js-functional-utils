70dfb9abd34f18ed0498ae559eea82e2354a7896
Sir-Kyle-Richardson <richardson.a.kyle.95@gmail.com>
2021-09-17 13:49:16 -0400

    Back to manual exports for intellisense

--- build/index.js
+++ build/index.js
@@ -1,19 +1,29 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var file_1 = require("./src/file");
-var exportAllCallback = function (file) {
-    var exports = require(file);
-    module.exports = __assign(__assign({}, module.exports), exports);
-};
-file_1.recursiveDirectoryWalk(__dirname + "/src", exportAllCallback);
+__exportStar(require("./src/array"), exports);
+__exportStar(require("./src/boolean"), exports);
+__exportStar(require("./src/curry"), exports);
+__exportStar(require("./src/file"), exports);
+__exportStar(require("./src/if-it"), exports);
+__exportStar(require("./src/is-empty"), exports);
+__exportStar(require("./src/lambda-it"), exports);
+__exportStar(require("./src/math"), exports);
+__exportStar(require("./src/number"), exports);
+__exportStar(require("./src/object"), exports);
+__exportStar(require("./src/pipe"), exports);
+__exportStar(require("./src/promised-timeout"), exports);
+__exportStar(require("./src/recursive-object-walk"), exports);
+__exportStar(require("./src/return-it"), exports);
+__exportStar(require("./src/set-value"), exports);
+__exportStar(require("./src/string"), exports);
+__exportStar(require("./src/types"), exports);
--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
-import { recursiveDirectoryWalk } from './src/file';
-
-const exportAllCallback = (file) => {
-    const exports = require(file);
-    module.exports = {
-        ...module.exports,
-        ...exports,
-    };
-};
-
-recursiveDirectoryWalk(`${__dirname}/src`, exportAllCallback);
+export * from './src/array';
+export * from './src/boolean';
+export * from './src/curry';
+export * from './src/file';
+export * from './src/if-it';
+export * from './src/is-empty';
+export * from './src/lambda-it';
+export * from './src/math';
+export * from './src/number';
+export * from './src/object';
+export * from './src/pipe';
+export * from './src/promised-timeout';
+export * from './src/recursive-object-walk';
+export * from './src/return-it';
+export * from './src/set-value';
+export * from './src/string';
+export * from './src/types';
