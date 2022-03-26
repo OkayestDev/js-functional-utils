c4cd45df777069874e0784c86dd21c1e1d78a8fe
Sir-Kyle-Richardson <richardson.a.kyle.95@gmail.com>
2022-03-26 03:04:02 -0400

    Testing husky hook

--- src/function-proxy.ts
+++ src/function-proxy.ts
@@ -1,7 +1,22 @@
 import { curry, curryObj } from './curry';
+import { TAnyFunction } from './types/any-function.type';
 
 export const PARAMS = 'PARAMS';
-export const RESPONSE = 'RESPONSE`';
+export const RESPONSE = 'RESPONSE';
+
+interface IFuncConfig {
+    logger?: (...args: any[]) => any;
+    isCurry?: boolean;
+    isObjectCurry?: boolean;
+}
+
+const GLOBAL_CONFIG: IFuncConfig = {
+    logger: console.log,
+};
+
+export const setGlobalConfig = (config: IFuncConfig) => {
+    Object.assign(GLOBAL_CONFIG, config);
+};
 
 const isLog = (): boolean => process.env.FUNC_IS_LOG === 'true';
 
@@ -9,37 +24,56 @@ export const setIsLog = (isLog: boolean) => {
     process.env.FUNC_IS_LOG = String(isLog);
 };
 
-const DEFAULT_CONFIG = Object.freeze({
-    logger: console.log,
-});
-
-// @todo global config?
-interface IFuncConfig {
-    logger: (...args: any[]) => any;
-    isCurry?: boolean;
-    isObjectCurry?: boolean;
-}
-
-const handleLog = (config: IFuncConfig) => (prefix: string, logItem: any) => {
+const handleLog = (config?: IFuncConfig) => (prefix: string, logItem: any) => {
     if (isLog()) {
         Promise.resolve(logItem).then((resolvedLogItem) => {
-            config.logger(prefix, resolvedLogItem);
+            config?.logger?.(prefix, resolvedLogItem);
         });
     }
 };
 
-const funcProxyHandler = (config: IFuncConfig) => ({
-    apply(targetFn: Function, _, args: any[]) {
+const applyConfigModifications = <T extends TAnyFunction>(fn: T, config?: IFuncConfig): T => {
+    if (config?.isCurry) {
+        return curry(fn) as T;
+    }
+
+    if (config?.isObjectCurry) {
+        return curryObj(fn) as T;
+    }
+
+    return fn;
+};
+
+const funcProxyHandler = (config?: IFuncConfig, additionalLogParams: any[] = []) => ({
+    apply<T extends Function>(targetFn: T, _, args: any[]) {
         const logger = handleLog(config);
-        logger(PARAMS, args);
+
+        const handler = (response) => {
+            if (typeof response === 'function') {
+                return func(response, config, [...additionalLogParams, ...args]);
+            }
+
+            if (response instanceof Promise) {
+                return Promise.resolve(response).then(handler);
+            }
+
+            logger(PARAMS, [...additionalLogParams, ...args]);
+            logger(RESPONSE, response);
+            return response;
+        };
+
         const response = targetFn(...args);
-        logger(RESPONSE, response);
-        return response;
+        return handler(response);
     },
 });
 
-const func = (fn: Function, config: IFuncConfig = DEFAULT_CONFIG) => {
-    return new Proxy(fn, funcProxyHandler(config));
+const func = <T extends TAnyFunction>(
+    fn: T,
+    config?: IFuncConfig,
+    additionalLogParams: any[] = []
+) => {
+    const modifiedFn = applyConfigModifications(fn, config);
+    return new Proxy<T>(modifiedFn, funcProxyHandler(config, additionalLogParams));
 };
 
 export default func;
