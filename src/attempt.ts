ba538fec27b7a02bdfb6e8e04bc2fcb78ba40c4a
Kyle Richardson <richardson.a.kyle.95@gmail.com>
2024-01-23 14:07:39 -0500

    Better typing for attempt

--- src/attempt.ts
+++ src/attempt.ts
@@ -1,25 +1,23 @@
 import { AnyFunction } from './types/utility-types.type';
 
-type AttemptResponse<T> = [T | undefined, Error | undefined];
+type AttemptResponse<T extends AnyFunction, G> = ReturnType<T> extends Promise<any>
+    ? Promise<[Awaited<G> | undefined, Error | undefined]>
+    : [G | undefined, Error | undefined];
 
-export function attempt<T extends AnyFunction>(callback: T) {
-    return (
-        ...args: Parameters<T>
-    ): AttemptResponse<ReturnType<T>> | Promise<AttemptResponse<ReturnType<T>>> => {
+export function attempt<T extends AnyFunction, G extends ReturnType<T>>(callback: T) {
+    return (...args: Parameters<T>): AttemptResponse<T, G> => {
         try {
             const callbackResponse = callback(...args);
 
             if (callbackResponse instanceof Promise) {
                 return callbackResponse
                     .then((val) => [val, undefined])
-                    .catch((error) => [undefined, error]) as Promise<
-                    AttemptResponse<ReturnType<T>>
-                >;
+                    .catch((error) => [undefined, error]) as AttemptResponse<T, G>;
             }
 
-            return [callbackResponse, undefined];
+            return [callbackResponse, undefined] as AttemptResponse<T, G>;
         } catch (error) {
-            return [undefined, error as Error];
+            return [undefined, error as Error] as AttemptResponse<T, G>;
         }
     };
 }
